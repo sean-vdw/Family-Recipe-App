cd176c775ddad3ca9e828d5642d90b93f4de0106
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-03-01 11:02:54 -0800

    Header component completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -26,15 +26,15 @@ export default function Header() {
               </Popover.Button>
             </div>            
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link to="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                 Sign in
-              </a>
-              <a
-                href="#"
+              </Link>
+              <Link
+                to="/register"
                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-cyan-600 hover:bg-cyan-700"
               >
                 Sign up
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -72,17 +72,17 @@ export default function Header() {
               </div>
               <div className="py-6 px-5 space-y-6">
                 <div>
-                  <a
-                    href="#"
+                  <Link
+                    to="/register"
                     className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-cyan-600 hover:bg-cyan-700"
                   >
                     Sign up
-                  </a>
+                  </Link>
                   <p className="mt-6 text-center text-base font-medium text-gray-500">
-                    Existing customer?
-                    <a href="#" className="text-cyan-600 hover:text-cyan-500">
+                    Existing customer? 
+                    <Link to="/login" className="text-cyan-600 hover:text-cyan-500">
                       Sign in
-                    </a>
+                    </Link>
                   </p>
                 </div>
               </div>
@@ -103,12 +103,12 @@ export default function Header() {
             </p>
             <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
-                <a
-                  href="#"
+                <Link
+                  to="/register"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 md:py-4 md:text-lg md:px-10"
                 >
                   Get started
-                </a>
+                </Link>
               </div>
             </div>
           </div>
