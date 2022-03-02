1b21884eb70bb980e3d16bd170b9f2e439260fc6
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-03-02 15:37:42 -0800

    added path for heroku server

--- api/server.js
+++ api/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const path = require('path');
 
 const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
 const restricted = require('./middleware/restricted');
 
+server.use(express.static(path.join(__dirname, '../client')));
 server.use(express.json());
 server.use(cors());
 
 server.use('/api/users', usersRouter);
 server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
 server.use((err, req, res, next) => {
   res.status(500).json({
