1a54cb0f2be5d303f62dafc7a6b842d38f25874d
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-03-03 08:54:10 -0800

    moved index to root

--- api/server.js
+++ api/server.js
@@ -2,22 +2,16 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
 const restricted = require('./middleware/restricted');
 
-server.use(express.static(path.join(__dirname, '../client/src')));
 server.use(express.json());
 server.use(cors());
 
 server.use('/api/users', usersRouter);
 server.use('/api/auth', authRouter);
-
-server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
-});
 
 server.use((err, req, res, next) => {
   res.status(500).json({
